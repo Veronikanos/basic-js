37813e8f186f35093af493ceba31da92043912f5
Veronica <vveronikanika@gmail.com>
2023-04-14 14:39:00 +0300

    fix: change check for what-season

--- src/what-season.js
+++ src/what-season.js
@@ -15,20 +15,9 @@ function getSeason(date) {
   if (!date) return 'Unable to determine the time of year!';
   if (Object.prototype.toString.call(date) !== '[object Date]')
     throw Error('Invalid date!');
-  // if (!date || !(date instanceof Date) || isNaN(date))
-  //   throw new Error('Invalid date!');
-  if (date.toString() == Date.prototype.toString.call(new Date()))
-    throw Error('Invalid date!');
+  if (Object.getOwnPropertyNames(date).length > 0)
+    throw new Error('Invalid date!');
 
-  // if (Object.getPrototypeOf(date) === Object.prototype)
-  //   throw Error('Invalid date!');
-  // if (date instanceof Date === false && isNaN(date))
-  //   throw Error('Invalid date!');
-  // // return 'Invalid date!';
-
-  // // throw new NotImplementedError('Not implemented');
-  // // npm run test ./test/what-season.test.js
-  // // remove line with error and write your code here
   const month = date.getMonth();
   if (month === 11 || month <= 1) return 'winter';
   if (month >= 2 && month < 5) return 'spring';
